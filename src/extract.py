e61ecddc2506260905c04a2db33e7fd800aa0a0c
a-oirgari <abdrahmanghali1998@gmail.com>
2026-09-15 14:31:07 +0100

    chore: la création du fichier /src/extract.py et l'exploration et l'analyse du données dans /notebooks/01_eda.ipynb

---
+++ src/extract.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+COLUMN_MAPPING = {
+    "Row ID": "row_id",
+    "Order ID": "order_id",
+    "Order Date": "order_date",
+    "Ship Date": "ship_date",
+    "Ship Mode": "ship_mode",
+    "Customer ID": "customer_id",
+    "Customer Name": "customer_name",
+    "Segment": "segment",
+    "Country": "country",
+    "City": "city",
+    "State": "state",
+    "Postal Code": "postal_code",
+    "Region": "region",
+    "Product ID": "product_id",
+    "Category": "category",
+    "Sub-Category": "sub_category",
+    "Product Name": "product_name",
+    "Sales": "sales",
+    "Quantity": "quantity",
+    "Discount": "discount",
+    "Profit": "profit",
+}
+
+def read_source_csv(csv_path):
+    df = pd.read_csv(csv_path, encoding="latin-1", dtype=str)
+    return df
+
+
+def rename_columns_for_staging(df):
+    return df.rename(columns=COLUMN_MAPPING)
+
+
